35b5a34f06d5736d79c6f0e36c93b4ed6caa3be8
rummolprod999 <rummolprod999@gmail.com>
2019-10-16 11:48:47 +0300

    oop

--- run.py
+++ run.py
@@ -7,10 +7,10 @@ import router
 def main():
     init_service.initializator()
     logging.info("start")
-    logging.info("end")
 
+
+main()
 
 if __name__ == '__main__':
-    main()
     router.app.run(debug=True, host='0.0.0.0')
     # model_by_inn.return_arb('5835115265')

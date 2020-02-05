5ce64ed93ee4d660d1c6ef63be08fdd76afe8da9
rummolprod999 <rummolprod999@gmail.com>
2020-02-05 09:29:11 +0300

    refractoring

---
+++ config.py
@@ -0,0 +1,28 @@
+API_KEY = 'e56ec10c617cc26de42f331d67b42a4c'
+
+all_items = True
+extends_fields = True
+base_headers = {'Pragma': 'no-cache', 'Accept-Encoding': 'gzip, deflate',
+                'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+                'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.93 Safari/537.36 Vivaldi/2.8.1664.40',
+                'Accept': 'application/json, text/javascript, */*',
+                'Referer': 'http://kad.arbitr.ru/',
+                'X-Requested-With': 'XMLHttpRequest',
+                'Cookie': 'ARRAffinityKAD=7baa4e65671c1a060ed43b95c488704e85e649305b65b2b1cb01a57b9b6a1e46; ASP.NET_SessionId=b5egnne1rrvh5kzfalcfs10k; CUID=b30d2509-c052-42da-b306-239872cc1c5b:KJHN9Hwc5VDFU22+vwbU3Q==; Notification_All=42bb419d7f084008b73cc949c3bd36bb_1581078000000_shown; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; wasm=3e266a13af4235ece42ac3256bae6980; rcid=8e7f0c12-ef95-4bf3-bd45-1ace4313122b',
+                'Connection': 'keep-alive',
+                'Cache-Control': 'no-cache'}
+
+instance_headers = {
+    'Cookie': 'ARRAffinityKAD=7baa4e65671c1a060ed43b95c488704e85e649305b65b2b1cb01a57b9b6a1e46; ASP.NET_SessionId=b5egnne1rrvh5kzfalcfs10k; CUID=b30d2509-c052-42da-b306-239872cc1c5b:KJHN9Hwc5VDFU22+vwbU3Q==; Notification_All=42bb419d7f084008b73cc949c3bd36bb_1581078000000_shown; SL_GWPT_Show_Hide_tmp=1; SL_wptGlobTipTmp=1; wasm=3e266a13af4235ece42ac3256bae6980; rcid=8e7f0c12-ef95-4bf3-bd45-1ace4313122b',
+    'Origin': 'http://kad.arbitr.ru',
+    'x-date-format': 'iso',
+    'Accept-Language': 'ru-RU,ru;q=0.9,en-US;q=0.8,en;q=0.7',
+    'X-Requested-With': 'XMLHttpRequest',
+    'Accept-Encoding': 'gzip, deflate',
+    'Connection': 'keep-alive',
+    'Pragma': 'no-cache',
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/77.0.3865.93 Safari/537.36 Vivaldi/2.8.1664.40',
+    'Content-Type': 'application/json',
+    'Accept': '*/*',
+    'Cache-Control': 'no-cache',
+    'Referer': 'http://kad.arbitr.ru/'}
